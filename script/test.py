467da946f3c504a12cdc1e85fe95a55e701c4e7b
Remus <remusoctavian100@gmail.com>
2018-11-12 19:36:47 +0200

    restructurare

---
+++ script/test.py
@@ -0,0 +1,10 @@
+print ("text")
+
+
+# git init
+# git remote add origin https://github.com/Remus100/my_first_project.git
+# git push -u origin master
+
+# modifici fisiere(modificari locale) -> git add .
+#                                     -> git commit (pas de verificare, cauta conflicte. ca sa editezi in editor apasa 'i' -> ca sa salvezi si iesi, apasa ESC + :wq )
+#                                     -> git push origin <branch>
--- test.py
+++
@@ -1,8 +0,0 @@
-print ("text")
-
-
-# git init
-# git remote add origin https://github.com/Remus100/my_first_project.git
-# git push -u origin master
-
-# modifici fisiere(modificari locale) -> git add . -> git commit (pas de verificare, cauta conflicte) -> git push origin <branch>  
